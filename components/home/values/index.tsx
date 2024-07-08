c8ca19980f6cddde6f7f8b1ba359c4e1ad6ce898
Poxie <poxen12@hotmail.com>
2024-07-08 20:18:29 +0200

    Add animation to values header

--- components/home/values/index.tsx
+++ components/home/values/index.tsx
@@ -1,15 +1,31 @@
+"use client";
 import SectionHeader from "@/components/section-header";
 import HomeValuesList from "./HomeValuesList";
+import { useRef } from "react";
+import useAnimateIntoView from "@/hooks/useAnimateIntoView";
 
 export default function HomeValues() {
+    const headerRef = useRef<HTMLDivElement>(null);
+    const subHeaderRef = useRef<HTMLSpanElement>(null);
+
+    const { initialState } = useAnimateIntoView(headerRef, { threshold: .9, hideOnScroll: true });
+    useAnimateIntoView(subHeaderRef, { siblingRef: headerRef, threshold: .9, delay: 100, hideOnScroll: true });
+
     return(
         <section className="p-section">
             <div className="mb-12 main-width text-center">
-                <SectionHeader>
+                <SectionHeader
+                    style={initialState}
+                    ref={headerRef}
+                >
                     Improving Today
                 </SectionHeader>
-                <span className="mt-4 block text-lg">
-                    Genom att vara..
+                <span 
+                    className="mt-2 block text-lg"
+                    style={initialState}
+                    ref={subHeaderRef}
+                >
+                    Genom att vara...
                 </span>
             </div>
             <HomeValuesList />
--- components/section-header/index.tsx
+++ components/section-header/index.tsx
@@ -1,18 +1,28 @@
+import React from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function SectionHeader({ children, className }: {
+type Props = {
     children: React.ReactNode;
     className?: string;
-}) {
+    style?: React.CSSProperties;
+};
+
+const SectionHeader = React.forwardRef<HTMLDivElement, Props>(({ children, className, style }, ref) => {
     return(
-        <div className={twMerge(
-            "flex justify-center",
-            className,
-        )}>
+        <div 
+            ref={ref}
+            style={style}
+            className={twMerge(
+                "flex justify-center",
+                className,
+            )}
+        >
             <h2 className="relative px-8 text-2xl sm:text-3xl text-center uppercase font-semibold tracking-widest bg-light-secondary">
                 {children}
                 <div className="bg-dark w-[calc(100%+3rem)] h-[1px] -z-[1] absolute left-2/4 top-2/4 -translate-y-2/4 -translate-x-2/4" />
             </h2>
         </div>
     )
-}
+});
+
+export default SectionHeader;
